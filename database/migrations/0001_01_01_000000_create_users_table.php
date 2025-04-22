69f727fe90b9bfc656351fdc6fd78d9a4ce2fa85
hamid <hamidmidoun060@gmail.com>
2025-04-22 13:13:26 +0100

    Remove unused user fields from migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,10 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
-            $table->string('driver_license')->nullable();
-            $table->boolean('id_admin')->default(false);
 
             $table->rememberToken();
             $table->timestamps();
