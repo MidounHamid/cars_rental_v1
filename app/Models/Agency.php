bfb65deea1e9636eef1cbea2cb68fe67d3382598
hamid <hamidmidoun060@gmail.com>
2025-05-18 00:34:30 +0100

    feat: Add RedirectIfAuthenticated middleware to handle authenticated user redirection

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Agency extends Model
 {
     use HasFactory;
-
+     protected $table = 'agencies';
     protected $fillable = [
         'name',
         'city',
